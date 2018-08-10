732cd56c4d56a47278e6cf7033056a22869704a5
Amanda Cameron <amanda.cameron103@gmail.com>
2018-08-10 23:28:31 +0000

    fixed server

--- server.py
+++ server.py
@@ -385,6 +385,5 @@ if __name__ == '__main__':
     # app.config['DEBUG'] = True
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     # app.config['SERVER_NAME'] = "showspotter.org:5000"
-    app.config['SERVER_NAME'] = "showspotter.org:5000"
     # DebugToolbarExtension(app)
     app.run(host='0.0.0.0')
